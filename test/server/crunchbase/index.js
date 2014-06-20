b150735c7f43d4e32c00c1303abc94e5d0dc864a
Tara Lerias <tlerias@gmail.com>
2014-06-20 09:34:12 -0400

    now we can query the Crunchbase API to grab all people & their IDs

--- test/server/crunchbase/index.js
+++ test/server/crunchbase/index.js
@@ -6,7 +6,7 @@ var should = require('should'),
 
 describe('GET /crunchbase/people', function() {
 
-  it('should respond with 200', function(done) {
+  xit('should respond with 200', function(done) {
     request(app)
       .get('/api/crunchbase/people')
       .expect(200)
@@ -17,19 +17,33 @@ describe('GET /crunchbase/people', function() {
       });
   });
 
-  it('should have "items" with a "name"', function(done) {
+
+  xit('should have "items" with a "name"', function(done) {
     request(app)
       .get('/api/crunchbase/people')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        var parsedBody = JSON.parse(res.body);
+        var parsedBody = res.body;
         parsedBody.data.should.be.instanceof(Object);
         parsedBody.data.should.have.property("items");
         parsedBody.data.items.should.be.instanceof(Array);
         parsedBody.data.items[0].should.have.property("name");
 
+        done();
+      });
+  });
+
+  xit('if theres more than one page, hit the route for every page', function(done) {
+    request(app)
+      .get('/api/crunchbase/people')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        var parsedBody = res.body;
+        parsedBody.data.should.have.property("paging");
         done();
       });
   });
