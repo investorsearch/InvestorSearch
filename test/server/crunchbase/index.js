7bca808f248ecab978418dd06bff0900591da7dd
Tara Lerias <tlerias@gmail.com>
2014-06-19 20:28:26 -0400

    test where GET crunchbase/people should return an object

--- test/server/crunchbase/index.js
+++ test/server/crunchbase/index.js
@@ -6,7 +6,7 @@ var should = require('should'),
 
 describe('GET /crunchbase/people', function() {
 
-  it('should respond with an object', function(done) {
+  it('should respond with 200', function(done) {
     request(app)
       .get('/api/crunchbase/people')
       .expect(200)
@@ -14,6 +14,18 @@ describe('GET /crunchbase/people', function() {
       .end(function(err, res) {
         if (err) return done(err);
         //res.body.should.be.instanceof(Object);
+        done();
+      });
+  });
+
+  it('should respond with an object', function(done) {
+    request(app)
+      .get('/api/crunchbase/people')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
         done();
       });
   });
