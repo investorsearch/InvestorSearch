a8b6ec331a488c9af9e1e5176f5215327cc18041
Tara Lerias <tlerias@gmail.com>
2014-06-25 11:54:12 -0400

    front end form working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ angular.module('investorSearchApp', [
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
-        controller: 'MainCtrl'
+        controller: 'SearchCtrl'
       })
       .when('/login', {
         templateUrl: 'partials/login',
@@ -28,9 +28,9 @@ angular.module('investorSearchApp', [
       .otherwise({
         redirectTo: '/'
       });
-      
+
     $locationProvider.html5Mode(true);
-      
+
     // Intercept 401s and redirect you to login
     $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
       return {
@@ -50,7 +50,7 @@ angular.module('investorSearchApp', [
 
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$routeChangeStart', function (event, next) {
-      
+
       if (next.authenticate && !Auth.isLoggedIn()) {
         $location.path('/login');
       }
