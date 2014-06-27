1eb755d1e81cbc46f19017cf661a6e56bae75ba1
Tara Lerias <tlerias@gmail.com>
2014-06-27 14:07:37 -0400

    autocomplete working for both companies and tags

--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -6,78 +6,15 @@ angular.module('investorSearchApp')
     var Search = {
       // hit route that queries MySQL here:
       getInvestors: function(constraints){
-        console.log("constraints")
-        console.log(constraints)
+        console.log("constraints");
+        console.log(constraints);
         var getInvestorsPromise = $http.post('/api/search', constraints).success(function(investors, status) {
           return investors;
         });
         return getInvestorsPromise;
-      },
-      fakeInvestors: function(){
-        return [{
-            "name": "Luben Pampoulov",
-            "id": 92683,
-            "bio": "Co-founder, Partner at GSV Asset Management",
-            "angellist_url": "https://angel.co/luben-pampoulov",
-            "image": "https://s3.amazonaws.com/photos.angel.co/users/92683-medium_jpg?1328170738",
-            "linkedin_url": "http://www.linkedin.com/in/lubenpampoulov",
-            "locations": [
-                {
-                    "id": 13256,
-                    "tag_type": "LocationTag",
-                    "name": "woodside, ca",
-                    "display_name": "Woodside",
-                    "angellist_url": "https://angel.co/woodside-ca-2"
-                }
-            ],
-            "roles": [
-                {
-                    "id": 14721,
-                    "tag_type": "RoleTag",
-                    "name": "institutional investor",
-                    "display_name": "Institutional Investor",
-                    "angellist_url": "https://angel.co/institutional-investor-1"
-                }
-            ],
-            "skills": [],
-            "investor": true
-        },
-        {
-            "name": "Kevin Hale",
-            "id": 168478,
-            "bio": "Partner at @y-combinator. Founded @wufoo (Acquired by @surveymonkey). Investor in @crowdtilt, @zapier, @strikingly and @lob ",
-            "angellist_url": "https://angel.co/kevinhale",
-            "image": "https://s3.amazonaws.com/photos.angel.co/users/168478-medium_jpg?1346439329",
-            "linkedin_url": "http://www.linkedin.com/in/unfoldedorigami",
-            "locations": [
-                {
-                    "id": 1692,
-                    "tag_type": "LocationTag",
-                    "name": "san francisco",
-                    "display_name": "San Francisco",
-                    "angellist_url": "https://angel.co/san-francisco"
-                },
-                {
-                    "id": 1681,
-                    "tag_type": "LocationTag",
-                    "name": "silicon valley",
-                    "display_name": "Silicon Valley",
-                    "angellist_url": "https://angel.co/silicon-valley"
-                }
-            ],
-            "roles": [
-                {
-                    "id": 9300,
-                    "tag_type": "RoleTag",
-                    "name": "angels",
-                    "display_name": "Angel",
-                    "angellist_url": "https://angel.co/angels"
-                }
-            ]
-          }
-        ]
       }
     }
     return Search;
+
 
   });
