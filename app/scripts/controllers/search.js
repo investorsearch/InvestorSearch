8fd329ef888e7a4f4141a78b3b852f06dec23001
Tara Lerias <tlerias@gmail.com>
2014-06-29 15:09:10 -0400

    front end

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -6,6 +6,51 @@ angular.module('investorSearchApp')
     $scope.selectedCompanies = [];
     $scope.selectedMarkets = [];
 
+    $scope.investors = [
+      {
+        id: 1,
+        al_id: 1,
+        name: "Marc Andreesen",
+        is_angel: 1,
+        location: "San Francisco, CA",
+        type: "Investor",
+        bio: "I invest in things",
+        image: 'https://s3.amazonaws.com/photos.angel.co/users/727-medium_jpg?1294803750'
+      },
+      {
+        id: 1,
+        al_id: 1,
+        name: "Marc Andreesen",
+        is_angel: 1,
+        location: "San Francisco, CA",
+        type: "Investor",
+        bio: "I invest in things",
+        image: 'https://s3.amazonaws.com/photos.angel.co/users/727-medium_jpg?1294803750'
+
+      },
+      {
+        id: 3,
+        al_id: 3,
+        name: "Institutional Venture Partners",
+        is_angel: null,
+        location: "San Francisco, CA",
+        type: "VC",
+        bio: "I invest in things",
+        image: 'https://s3.amazonaws.com/photos.angel.co/startups/i/46542-50efe21ce554564a23ee8d5fbcdf8491-medium_jpg.jpg?buster=1334276847'
+      },
+      {
+        id: 2,
+        al_id: 2,
+        name: "Preston Ford",
+        is_angel: 1,
+        location: "Boston",
+        type: "Investor",
+        bio: "Founder @The Longview Group, @BlockCross Holdings • Studied at @MIT, @HBS",
+         image: 'https://s3.amazonaws.com/photos.angel.co/users/133774-medium_jpg?1403116229'
+      },
+
+    ]
+
     // $scope.addField = function() {
     //   $scope.constraints.push({text: ''});
     // };
@@ -13,6 +58,7 @@ angular.module('investorSearchApp')
     //   $scope.constraints.splice(index, 1);
     // };
     $scope.search = function(){
+      //------------------------------- REAL SEARCH FUNCTION BELOW:
       console.log("companies in search");
       console.log($scope.companies);
       var constraints = {
@@ -25,6 +71,16 @@ angular.module('investorSearchApp')
         $scope.investors = investorsFromPromise.data;
         console.log($scope.investors);
       });
+
+    //---------------------------- FAKE SEARCH FUNCTION BELOW TO TEST RANKING
+
+    // $scope.investors = [];
+    // for(var i = 0; i < fake_investors.length; i++){
+    //   if($scope.investors.indexOf(fake_investors[i]) === -1){
+    //     $scope.investors.push(fake_investors[i]);
+    //   } else if (fake_investors[i] === $scope.investors)
+    // }
+
     };
 
     $scope.completeCompanies = function(value) {
