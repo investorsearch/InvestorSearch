bf8255efea2c7d03cb2e105dde0fcaba3d660b6e
Tara Lerias <tlerias@gmail.com>
2014-07-02 18:07:05 -0400

    added search feature

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -6,6 +6,7 @@ angular.module('investorSearchApp')
     $scope.selectedCompanies = [];
     $scope.selectedMarkets = [];
     $scope.investors = [];
+    $scope.querySearch = '';
 
     List.show().then(function(list){
       $scope.savedInvestors = list.data;
